e0f9abcde4741ff2b06f29ce97252d028c59ebe4
em.ill.ozev <emilfrogy@gmail.com>
2018-11-09 23:56:21 +0000

    Added mocha Added 404 and general error middleware handler features

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+'use strict'
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.json('Hello there');
 });
 
+
+app.use((req, res, next) => {
+    let error = new Error("404 Not Found! Resource doesn\'t exists!");
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({errorCode: err.status, message: err.message});
+});
+
 app.listen(port, () => {
-   console.log("The server is running on port " + port);
+    console.log("Express is running on port: " + port);
 });
