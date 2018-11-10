a4b28f877ea2295f5881d264992575da7c6eb235
em.ill.ozev <emilfrogy@gmail.com>
2018-11-10 00:12:35 +0000

    Set up of /users URL

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const port = process.env.PORT || 3000;
+const routes = require("./routes/index");
 
-app.get('/', (req, res) => {
-    res.json('Hello there');
-});
-
+app.use('/', routes);
 
 app.use((req, res, next) => {
     let error = new Error("404 Not Found! Resource doesn\'t exists!");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const userRouter = require("./users");
+router.use('/users', userRouter);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ message: 'Get /' });
 });
 
 module.exports = router;
